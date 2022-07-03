1dd080803635032ff2fda83a61286f5c95541f0c
Rishiraj-singh7 <rsshekhawat795@gmail.com>
2022-07-03 09:48:24 +0530

    layout

--- main.py
+++ main.py
@@ -5,9 +5,43 @@ from pyqt5.QtWenEngineWidgets import *
 
 class MyWebBrowser(QMainWindow):
 
-    def ___init__(self):
+    def ___init__(self,*args, **kwargs):
         super(MyWebBrowser,self).__init__(*args, **kwargs)
 
-        self.Window = QtWidget()
+        self.Window = QWidget()
 
         self.Window.setWindowTitle("NeuralNine Web Browser")
+
+        #ui elements
+
+        self.layout= QVBoxLayout()
+        self.horizontal  = QHBoxLayout()
+        
+        self.url_bar = QTextEdit()
+        self.url_bar.setMaximuMHeight(30)
+
+        self.go_btn =QPushButton("Go")
+        self.go_btn.setMinmumHeight(30)
+
+        self.back_btn =QPushButton("<")
+        self.back_btn.setMinmumHeight(30)
+
+        self.forwad_btn =QPushButton(">")
+        self.forwad_btn.setMinmumHeight(30)
+
+        self.horizontal.addWidget(self.url_btn)
+        self.horizontal.addWidget(self.go_btn)
+        self.horizontal.addWidget(self.back_btn)
+        self.horizontal.addWidget(self.forwad_btn)
+
+        self.browser = QWebEngineView()
+
+       
+        self.layout.addLayout(self.horizontal)
+        self.layout.addWidget(self.browser)
+
+        self.browser.setUrl(QUrl("http://google.com"))
+
+        self.window.setLayout(self.layout)
+        self.window.show()
+
